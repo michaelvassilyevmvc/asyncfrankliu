bed87475917f139134aab939f5ef198d9c214764
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-29 20:41:05 +0500

    Ep6. Создана имитация работы веб сервера через создание потоков для получения запроса, отслеживания очереди запросов и выполнения запроса

--- WebServerAssignmentQuestion/Program.cs
+++ WebServerAssignmentQuestion/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Server is running. Type 'exit' to stop.");
+﻿Queue<string?> requestQueue = new Queue<string?>();
+
+// 2. Start the requests queue monitoring thread
+Thread monitorThread = new Thread(MonitorQueue);
+monitorThread.Start();
+
+Console.WriteLine("Server is running. Type 'exit' to stop.");
 while (true)
 {
     string? input = Console.ReadLine();
@@ -6,11 +12,27 @@ while (true)
     {
         break;
     }
-
-    ProcessInput(input);
+    requestQueue.Enqueue(input);
 }
 
-static void ProcessInput(string input)
+void MonitorQueue()
+{
+    while (true)
+    {
+        if (requestQueue.Count > 0)
+        {
+            string? input = requestQueue.Dequeue();
+            Thread processingThread = new Thread(() => ProcessInput(input));
+            processingThread.Start();
+        }
+        Thread.Sleep(100);
+        
+        
+    }
+}
+
+// 3. Processing the request
+void ProcessInput(string input)
 {
     Thread.Sleep(2000);
     Console.WriteLine($"Processed input: {input}");
